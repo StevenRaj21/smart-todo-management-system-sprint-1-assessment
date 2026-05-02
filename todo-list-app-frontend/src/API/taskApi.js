1dcd4a83d1d30c72f5dd589c873cb750622c975a
StevenRaj21 <Steven.raj@criskasecurity.com>
2026-05-02 08:54:26 +0530

    Fix API base URL using env

--- todo-list-app-frontend/src/API/taskApi.js
+++ todo-list-app-frontend/src/API/taskApi.js
@@ -1,79 +1,42 @@
-const BASE_URL = "http://localhost:5000/api";
-
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 // GET TASKS
 export const getTasks = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}/tasks`);
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch tasks");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.log("Error fetching tasks:", error);
-  }
+  const response = await fetch(`${BASE_URL}/tasks`);
+  if (!response.ok) throw new Error("Failed to fetch tasks");
+  return await response.json();
 };
-
 
 // CREATE TASK
 export const createTask = async (taskData) => {
-  try {
-    const response = await fetch(`${BASE_URL}/tasks`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(taskData),
-    });
+  const response = await fetch(`${BASE_URL}/tasks`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(taskData),
+  });
 
-    if (!response.ok) {
-      throw new Error("Failed to create task");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.log("Error creating task:", error);
-  }
+  if (!response.ok) throw new Error("Failed to create task");
+  return await response.json();
 };
-
 
 // UPDATE TASK
 export const updateTask = async (id, taskData) => {
-  try {
-    const response = await fetch(`${BASE_URL}/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(taskData),
-    });
+  const response = await fetch(`${BASE_URL}/tasks/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(taskData),
+  });
 
-    if (!response.ok) {
-      throw new Error("Failed to update task");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.log("Error updating task:", error);
-  }
+  if (!response.ok) throw new Error("Failed to update task");
+  return await response.json();
 };
-
 
 // DELETE TASK
 export const deleteTask = async (id) => {
-  try {
-    const response = await fetch(`${BASE_URL}/tasks/${id}`, {
-      method: "DELETE",
-    });
+  const response = await fetch(`${BASE_URL}/tasks/${id}`, {
+    method: "DELETE",
+  });
 
-    if (!response.ok) {
-      throw new Error("Failed to delete task");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.log("Error deleting task:", error);
-  }
+  if (!response.ok) throw new Error("Failed to delete task");
+  return await response.json();
 };
